9a1b19cb6c1fdfef5472e384e4dd45ea7f7bd552
Florent <florent@frizoncorrea.fr>
2025-10-11 09:53:51 +0200

    rak4631: enable building with sh1106 display

--- variants/rak4631/target.h
+++ variants/rak4631/target.h
@@ -10,6 +10,7 @@
 
 #ifdef DISPLAY_CLASS
   #include <helpers/ui/SSD1306Display.h>
+  #include <helpers/ui/SH1106Display.h>
   extern DISPLAY_CLASS display;
   #include <helpers/ui/MomentaryButton.h>
   extern MomentaryButton user_btn;
